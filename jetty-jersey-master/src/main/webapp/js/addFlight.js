f60b6ecb03101f3898e6b523b8daef3583ad5556
CANONICI MERLE ELIE <canonici@informatique.univ-paris-diderot.fr>
2016-03-07 02:03:11 +0100

    add flight

--- jetty-jersey-master/src/main/webapp/js/addFlight.js
+++ jetty-jersey-master/src/main/webapp/js/addFlight.js
@@ -1,8 +1,39 @@
 
-$("#template_anchor").submit(function(e){ // On sélectionne le formulaire par son identifiant
 
-    e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
-    var donnees = $(this).serialize(); // On créer une variable contenant le formulaire sérialisé
-    addServerData("http://localhost:8080/ws/flight",donnees);
+function string(e){
+    return '"' + e + '"';
+}
 
+function form(){
+    $("#add_form").submit(function(e){ // On sélectionne le formulaire par son identifiant
+	e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
+	var commercial_number = $("#commercial_number").val();
+	var atc_number = $("#atc_number").val();
+	var departure_airport = $("#departure_airport").val();
+	var arrival_airport = $("#arrival_airport").val();
+	var departure_date = $("#departure_date").val();
+	var departure_time = $("#departure_time").val();
+	var arrival_date = $("#arrival_date").val();
+	var arrival_time = $("#arrival_time").val();
+	var json_str =
+	    '{"commercial_number":' + string(commercial_number) +
+	    ',"ATC_code":' + string(atc_number) +
+	    ',"plane":null' +
+	    ',"crew_members":[]' +
+	    ',"departure_airport":' + string(departure_airport) +
+	    ',"arrival_airport":' + string(arrival_airport) +
+	    ',"departure_time":' + departure_date +
+	    ',"arrival_time":' + arrival_date +
+	    ',"ofp_url":null' +
+	    ',"weather_maps_url":null' +
+	    ',"notam":[]}';
+	//var donnees = $("#add_form").serialize(); // On créer une variable contenant le formulaire sérialisé
+	console.log(json_str);
+	var json = JSON.parse(json_str);
+	addServerData("/ws/flight",json);
+    });
+}
+
+$( document ).ready(function() {
+    form();
 });
--- jetty-jersey-master/src/main/webapp/js/flightlist_client.js
+++ jetty-jersey-master/src/main/webapp/js/flightlist_client.js
@@ -8,9 +8,9 @@ function flightTempGen(dataArray){
 
 function hydrateFlightList(data,template){
     var html = template({
-	"commercial_number":JSON.stringify(data.commercial_number),
-	"departure_airport":JSON.stringify(data.departure_airport),
-	"arrival_airport":JSON.stringify(data.arrival_airport),
+	"commercial_number":data.commercial_number,
+	"departure_airport":data.departure_airport,
+	"arrival_airport":data.arrival_airport,
 	"departure_time":JSON.stringify(data.departure_time),
 	"arrival_time":JSON.stringify(data.arrival_time),
 	"url": "/ws/flight/" + data.commercial_number
